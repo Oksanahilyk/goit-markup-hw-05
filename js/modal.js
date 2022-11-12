4d038edf80747506571550be1da9f21ab4711f84
Oksanahilyk <hilyk@ukr.net>
2022-11-12 13:37:23 +0200

    Додає css переходи
    Додане модальне вікно, зроблені переходи при ховері тіни карток в портфоліо

--- js/modal.js
+++ js/modal.js
@@ -7,8 +7,11 @@
  
 	refs.openModalBtn.addEventListener("click", toggleModal);
 	refs.closeModalBtn.addEventListener("click", toggleModal);
- 
+
+	 
 	function toggleModal() {
 	  refs.modal.classList.toggle("is-hidden");
 	}
+	
+
  })();
